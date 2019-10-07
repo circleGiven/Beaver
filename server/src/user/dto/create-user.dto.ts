6e5eafee3968e5966093e2e0e2b926ad530b3a68
circlegiven <circlegiven@gmail.com>
2019-10-07 14:04:43 +0900

    #3 Add DTO in user
    - set isValueEqual decorator to confirmPassword field
    - create update user DTO
    - add update user DTO in index

--- server/src/user/dto/create-user.dto.ts
+++ server/src/user/dto/create-user.dto.ts
@@ -1,5 +1,7 @@
 import { ApiModelProperty } from '@nestjs/swagger';
 import {IsEmail, IsNotEmpty} from 'class-validator';
+import {IsValueEqual} from '../../common/decorator/is-value-equal.decorator';
+import {DecoratorConstant} from '../../common/constants/decorator.constant';
 
 export class CreateUserDto {
     @ApiModelProperty()
@@ -17,5 +19,6 @@ export class CreateUserDto {
 
     @ApiModelProperty()
     @IsNotEmpty()
+    @IsValueEqual('password', {message: DecoratorConstant.INVALID_VALUE_EQUAL})
     readonly confirmPassword: string;
 }
---
+++ server/src/user/dto/update-user.dto.ts
@@ -0,0 +1,16 @@
+import { ApiModelProperty } from '@nestjs/swagger';
+
+export class UpdateUserDto {
+
+    @ApiModelProperty()
+    readonly name: string;
+
+    @ApiModelProperty()
+    readonly email: string;
+
+    @ApiModelProperty()
+    readonly password: string;
+
+    @ApiModelProperty()
+    readonly confirmPassword: string;
+}
