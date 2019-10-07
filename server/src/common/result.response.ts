c240c0e0a23a4e80a13949b54efe7c3ca4b3f08b
circlegiven <circlegiven@gmail.com>
2019-10-07 14:14:36 +0900

    #3 Change property name in result response

--- server/src/common/result.response.ts
+++ server/src/common/result.response.ts
@@ -1,13 +1,13 @@
 import * as _ from 'lodash';
 
 export class ResultResponse {
-    private response: string | object;
-    private status: number;
+    private result: string | object;
+    private statusCode: number;
     private message?: string;
 
-    constructor(response: string | object, status: number, message?: string) {
-        this.response = response;
-        this.status = status;
+    constructor(result: string | object, status: number, message?: string) {
+        this.result = result;
+        this.statusCode = status;
         // if not empty message
         if (!_.isNil(message)) {
             this.message = message;
