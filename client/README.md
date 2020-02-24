1c24f18e066428cf8cc2cb21dfd5141d16c7e723
circlegiven <circlegiven@gmail.com>
2020-02-24 11:40:10 +0900

    #14 Angular version up
    - Remove previous template

--- client/README.md
+++
@@ -1,3 +0,0 @@
-# Beaver
-
-Angular project
--- client/src/app/sign/password-reset.component.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from '@angular/core';
-
-@Component({
-  selector: 'component-password-reset',
-  templateUrl: 'password-reset.component.html'
-})
-export class PasswordResetComponent {
-
-}
