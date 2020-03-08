0d18acf35b8f21f7347a050615935045bdb7a240
circlegiven <circlegiven@gmail.com>
2020-03-08 11:09:18 +0900

    #15 Enable CORS

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,7 +3,9 @@ import { AppModule } from './app.module';
 import {setSwagger} from './config/swagger.config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+  });
   setSwagger(app);
   await app.listen(3000);
 }
