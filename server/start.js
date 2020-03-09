1cc86df74cd65cece34cd3b6363850d52c0114c7
circlegiven <circlegiven@gmail.com>
2020-03-09 11:34:15 +0900

    #15 Fix pm2 start setting

--- server/start.js
+++ server/start.js
@@ -2,11 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'Beaver-server',
-      script: 'dist/main.js',
-      instances: 1,
-      autorestart: true,
-      watch: false,
-      max_memory_restart: '1G',
+      script: './dist/main.js',
       env: {
         NODE_ENV: 'production',
         PORT: '3000'
