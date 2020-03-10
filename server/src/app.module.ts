58049dc1deecc8f6257488239ae724bdfcf68ac7
circlegiven <circlegiven@gmail.com>
2020-03-10 10:54:21 +0900

    #20 Change server config

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,18 +2,23 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import {UserModule} from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-      TypeOrmModule.forRoot({
-          type: 'mysql',
-          host: 'localhost',
-          port: 3306,
-          username: 'dev',
-          password: 'test1234',
-          database: 'beaver',
-          synchronize: true,
+      ConfigModule.forRoot({isGlobal: true}),
+      TypeOrmModule.forRootAsync({
+        imports: [ConfigModule],
+        inject: [ConfigService],
+        useFactory: async (configService: ConfigService) => ({
+          type: 'mysql' as 'mysql',
+          host: configService.get('DATABASE_HOST'),
+          port: configService.get<number>('DATABASE_PORT'),
+          username: configService.get('DATABASE_USER'),
+          password: configService.get('DATABASE_PASSWORD'),
+          database: configService.get('DATABASE_SCHEMA'),
           entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        }),
       }),
       UserModule,
       AuthModule,
